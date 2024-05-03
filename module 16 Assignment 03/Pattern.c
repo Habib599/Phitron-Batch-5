cae46a51518a4e6df6e2cc372e4f60751d3548d3
habib599 <mdhabiburr599@gmail.com>
2024-05-04 00:01:54 +0600

    hi

--- module 16 Assignment 03/Pattern.c
+++ module 16 Assignment 03/Pattern.c
@@ -1,42 +1,34 @@
 #include<stdio.h>
 int main()
 {
-   int n,s,k=1;
-   scanf("%d",&n);
-   s=n-1;
-   for (int i = 1;i<=n ; i++)
-   {
-        for (int j = 1; j<=s ; j++) 
-        {
-            printf(" ");
-        }
-        for (int j = 1; j <=k; j++) 
-        {
-            if (i%2==0) printf("-");
-            else printf("#");
-        }
-        k+=2;
-        s--;
-        printf("\n");
-   }
-    s=1;
-    k=n-1;
-    for (int i = 1;i<=n ; i++)
+    int n,s,k;
+    scanf("%d",&n);
+    s=n-1;
+    k=1;
+    for(int i=1;i<=(2*n)-1;i++)
     {
-        for (int j = 1; j<=s ; j++) 
+        
+        for(int j=1;j<=s;j++)
         {
             printf(" ");
         }
-        for (int j = 1; j <=k; j++) 
+        for(int j=1;j<=k;j++)
         {
             if (i%2==0) printf("-");
             else printf("#");
         }
-        k-=2;
-        s++;
+        
+        if(i<=n-1)
+        {
+            s--;
+            k=k+2;
+        }
+        else
+        {
+            s++;
+            k=k-2;
+        }
         printf("\n");
     }
-   
-   
-   return 0;
+    return 0;
 }
